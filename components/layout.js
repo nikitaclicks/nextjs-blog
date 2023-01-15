382a6f6da611294a8af29af4e6fcc1aef74e3f51
Nikita Dmitriev <nikitadmitry@gmail.com>
2023-01-15 23:12:38 +0100

    test preview build

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Nikita';
-export const siteTitle = 'Next.js Sample Website';
+const name = 'Nikita is playing with next js';
+export const siteTitle = 'Next.js Blog';
 
 export default function Layout({ children, home }) {
     return (
