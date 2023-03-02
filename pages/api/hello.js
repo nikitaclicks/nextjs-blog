c021de1f593c91ce0a7be3fca34e0543feb843d7
Nikita Dmitriev <nikitadmitry@gmail.com>
2023-03-02 18:25:59 +0100

    rm node workflow

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,6 @@
 export default function handler(req, res) {
     res.status(200).json({ 
         text: 'Hello there',
-        url: process.env.WEBSITE_RUN_FROM_PACKAGE,
         time: new Date()
      });
 }
