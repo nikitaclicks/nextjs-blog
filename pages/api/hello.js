f2c40311541b99e72cc3d9c9ce5ab2962861e8c9
Nikita Dmitriev <nikitadmitry@gmail.com>
2023-03-02 18:19:55 +0100

    standalone output

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
     res.status(200).json({ 
         text: 'Hello there',
+        url: process.env.WEBSITE_RUN_FROM_PACKAGE,
         time: new Date()
      });
 }
